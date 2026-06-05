e307eccedd1b8edad6fe3c333bd05bb864c99358
Alexia Ilia <ale.dia108@gmail.com>
2026-06-05 13:48:24 +0300

    Fix direct execution of Spark jobs

--- spark_jobs/prediction_job.py
+++ spark_jobs/prediction_job.py
@@ -1,3 +1,8 @@
+from pathlib import Path
+import sys
+
+sys.path.insert(0, str(Path(__file__).resolve().parents[1]))
+
 from app.dal.mongo_connection import db
 from pyspark.sql import SparkSession
 
